6078c663939a79047afb8c1b63acb71b60066df5
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-10-16 14:59:10 +0100

    Added index and name

--- app.rb
+++ app.rb
@@ -2,12 +2,7 @@ require 'sinatra'
 require 'shotgun'
 
 
-get '/' do
-  "Hello!"
-end
-
 get '/cat' do
-  "<div style='border: 3px dashed red'>
-    <img src='http://bit.ly/1eze8aE'>
-   </div>"
+  @name = ["Amigo", "Fluffy", "Oscar"].sample
+  erb(:index)
 end
